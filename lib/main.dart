021f2b05df72184cfae53593e23e8cc6407f8e03
Péricles Narbal <periclesnarbal@iMacdePericles.ciccr-ce.interno>
2020-09-30 09:42:22 -0300

    blank project

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  MaterialApp app = new MaterialApp(
+    title: "contador de pessoas",
+    home: Container(
+      color: Colors.white,
+    ),
+  );
+  runApp(app);
+}
