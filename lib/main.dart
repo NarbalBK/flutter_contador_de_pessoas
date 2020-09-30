f292c64190d39eb1ab41b7dff59d161fe84381e7
Péricles Narbal <periclesnarbal@iMac-de-Pericles.local>
2020-09-30 10:10:03 -0300

    texto estilizado e no centro da tela

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() {
   MaterialApp app = new MaterialApp(
-    title: "contador de pessoas",
-    home: Container(
-      color: Colors.white,
-    ),
-  );
+      title: "contador de pessoas",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "Pode entrar!",
+            style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0),
+          )
+        ],
+      ));
   runApp(app);
 }
